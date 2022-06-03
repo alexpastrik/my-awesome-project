4fa0ea89f1acf86a6e9e90e94659d330ff7dce5f
alexpastrik <102768664+alexpastrik@users.noreply.github.com>
2022-06-03 11:18:38 -0400

    revert to beginning to add week 8 changes

--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100,
-  },
-  moscow: {
-    temp: -5,
-    humidity: 20,
-  },
-};
-
 let city = prompt("Enter a city");
 city = city.trim();
 city.toLowerCase();
